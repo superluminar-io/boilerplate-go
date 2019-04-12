8907b0c01f564c56bb2c3da2cdb7e1f0cfd785ce
sbstjn <mail@sbstjn.com>
2019-04-12 09:15:27 +0200

    chore: Add @ to makefile

--- scripts/serverless.mk
+++ scripts/serverless.mk
@@ -10,21 +10,21 @@ FILE_TEMPLATE = ./template.yml
 FILE_PACKAGE = ./dist/stack.yml
 
 configure:
-	aws --profile $(AWS_PROFILE) s3api create-bucket \
+	@ aws --profile $(AWS_PROFILE) s3api create-bucket \
 		--bucket $(AWS_BUCKET_NAME) \
 		--region $(AWS_REGION) \
 		--create-bucket-configuration LocationConstraint=$(AWS_REGION)
 
 package:
 	@ mkdir -p dist
-	aws --profile $(AWS_PROFILE) cloudformation package \
+	@ aws --profile $(AWS_PROFILE) cloudformation package \
 		--template-file $(FILE_TEMPLATE) \
 		--s3-bucket $(AWS_BUCKET_NAME) \
 		--output-template-file $(FILE_PACKAGE) \
 		--region $(AWS_REGION)
 
 deploy:
-	aws --profile $(AWS_PROFILE) cloudformation deploy \
+	@ aws --profile $(AWS_PROFILE) cloudformation deploy \
 		--template-file $(FILE_PACKAGE) \
 		--region $(AWS_REGION) \
 		--capabilities CAPABILITY_IAM \
