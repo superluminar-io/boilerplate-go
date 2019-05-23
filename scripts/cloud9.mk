85afbd9c50f43aa9df9b7b18cf88bf33f9eb17ef
Jan Brauer <jan.brauer@superluminar.io>
2019-05-23 13:28:41 +0200

    Print instructions

--- scripts/cloud9.mk
+++ scripts/cloud9.mk
@@ -2,7 +2,8 @@ GOLANG_TARBALL = go1.12.5.linux-amd64.tar.gz
 
 .PHONY: cloud9
 cloud9:
-	wget -P /tmp https://dl.google.com/go/$(GOLANG_TARBALL)
-	sudo tar -C /usr/local -xzf /tmp/$(GOLANG_TARBALL)
-	echo 'export PATH=$$PATH:/usr/local/go/bin' >> ~/.bashrc
-	source ~/.bashrc
+	@ wget -P /tmp https://dl.google.com/go/$(GOLANG_TARBALL)
+	@ sudo tar -C /usr/local -xzf /tmp/$(GOLANG_TARBALL)
+	@ echo 'export PATH=$$PATH:/usr/local/go/bin' >> ~/.bashrc
+	@ echo 'Done. Now run the command below.'
+	@ echo 'source ~/.bashrc'
