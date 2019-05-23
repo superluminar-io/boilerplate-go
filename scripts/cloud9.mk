ef2b1e10b9d9e175b6f1d54cf8112a4097dc46bd
Jan Brauer <jan.brauer@superluminar.io>
2019-05-23 13:25:15 +0200

    Fix Makefile

--- scripts/cloud9.mk
+++ scripts/cloud9.mk
@@ -1,11 +1,8 @@
 GOLANG_TARBALL = go1.12.5.linux-amd64.tar.gz
 
-/tmp/$(GOLANG_TARBALL):
+.PHONY: cloud9
+cloud9:
 	wget -P /tmp https://dl.google.com/go/$(GOLANG_TARBALL)
-
-/usr/local/go: /tmp/$(GOLANG_TARBALL)
-	sudo tar -C /usr/local -xzf $@ 
-
-cloud9: /usr/local/go
-	echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-	. ~/.bashrc
+	sudo tar -C /usr/local -xzf /tmp/$(GOLANG_TARBALL)
+	echo 'export PATH=$$PATH:/usr/local/go/bin' >> ~/.bashrc
+	source ~/.bashrc
