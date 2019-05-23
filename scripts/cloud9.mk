634ea05d3ebef0f0122e92d7ed45ca9815244ecd
Jan Brauer <jan.brauer@superluminar.io>
2019-05-23 13:33:43 +0200

    Fix path

--- scripts/cloud9.mk
+++ scripts/cloud9.mk
@@ -4,6 +4,6 @@ GOLANG_TARBALL = go1.12.5.linux-amd64.tar.gz
 cloud9:
 	@ wget -P /tmp https://dl.google.com/go/$(GOLANG_TARBALL)
 	@ sudo tar -C /usr/local -xzf /tmp/$(GOLANG_TARBALL)
-	@ echo 'export PATH=$$PATH:/usr/local/go/bin' >> ~/.bashrc
+	@ echo 'export PATH=/usr/local/go/bin:$$PATH' >> ~/.bashrc
 	@ echo 'Done. Now run the command below.'
 	@ echo 'source ~/.bashrc'
